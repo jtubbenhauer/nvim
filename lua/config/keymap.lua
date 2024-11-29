0eb839461e58ca270854bb40d5667a36cee2a2be
Jack Tubbenhauer <jack@tubbs.dev>
2024-11-30 10:52:09 +1100

    clean up keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,97 +1,22 @@
 local set = vim.keymap.set
-local add = require("selfhelp").add
 local gs = require("gitsigns")
 
 -- Fzf
 
 set("n", "<leader>fz", ":lua require('fzf-lua').builtin({ winopts = { fullscreen=false, height=0.8, width=0.5 } })<cr>")
-add({
-	mode = "n",
-	lhs = "<leader>sf",
-	rhs = ":lua require('fzf-lua').files()<cr>",
-	desc = "Search files",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>ss",
-	rhs = ":lua require('fzf-lua').git_status()<cr>",
-	desc = "Search git status",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>sc",
-	rhs = ":lua require('utils').fzf_git_changes()<cr>",
-	desc = "Search git changes",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>qf",
-	rhs = ":lua require('fzf-lua').quickfix()<cr>",
-	desc = "Search quickfix",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>sg",
-	rhs = ":lua require('fzf-lua').live_grep()<cr>",
-	desc = "Search live grep",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>so",
-	rhs = ":lua require('fzf-lua').buffers()<cr>",
-	desc = "Search recent files",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>sa",
-	rhs = ":lua require('fzf-lua').resume()<cr>",
-	desc = "Resume last search",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>sd",
-	rhs = ":lua require('utils').fzf_dirs()<cr>",
-	desc = "Search directory",
-	category = "Search",
-})
-add({
-	mode = "n",
-	lhs = "<leader>si",
-	rhs = ":lua require('utils').grep_directory()<cr>",
-	desc = "Grep in directory",
-	category = "Search",
-})
-
-add({
-	mode = "n",
-	lhs = "<leader>qq",
-	rhs = ":SelfHelp<cr>",
-	desc = "Show help",
-	category = "General",
-})
+set("n", "<leader>sf", ":lua require('fzf-lua').files()<cr>")
+set("n", "<leader>ss", ":lua require('fzf-lua').git_status()<cr>")
+set("n", "<leader>sc", ":lua require('utils').fzf_git_changes()<cr>")
+set("n", "<leader>qf", ":lua require('fzf-lua').quickfix()<cr>")
+set("n", "<leader>sg", ":lua require('fzf-lua').live_grep()<cr>")
+set("n", "<leader>so", ":lua require('fzf-lua').buffers()<cr>")
+set("n", "<leader>sa", ":lua require('fzf-lua').resume()<cr>")
+set("n", "<leader>sd", ":lua require('utils').fzf_dirs()<cr>")
+set("n", "<leader>si", ":lua require('utils').grep_directory()<cr>")
 
 -- File browser
-add({
-	mode = "n",
-	lhs = "<leader>er",
-	rhs = ":Oil .<cr>",
-	desc = "Open file browser in root directory",
-	category = "File Browser",
-})
-add({
-	mode = "n",
-	lhs = "<leader>ed",
-	rhs = ":Oil<cr>",
-	desc = "Open file browser in current directory",
-	category = "File Browser",
-})
+set("n", "<leader>er", ":Oil .<cr>")
+set("n", "<leader>ed", ":Oil<cr>")
 
 -- LSP
 set("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>")
@@ -116,37 +41,19 @@ set({ "i", "v" }, "<C-s>", "<Esc><cmd>w<cr>")
 set("n", "<leader>vs", "<cmd>vs<CR><cmd>wincmd l<CR>")
 set("n", "<leader>ct", "<cmd>tabclose<CR>")
 set("n", "<leader>cb", "<cmd>q<cr>")
-add({ mode = "n", lhs = "<leader>ms", rhs = ":Messages messages<cr>", desc = "Show messages", category = "General" })
+set("n", "<leader>ms", ":Messages messages<cr>")
 set("n", "<C-e>", "2<C-e>")
 set("n", "<C-y>", "2<C-y>")
 set({ "n", "i", "v" }, "<C-h>", ":vertical resize -5<CR>")
 set({ "n", "i", "v" }, "<C-l>", ":vertical resize +5<CR>")
-add({ mode = "n", lhs = "<leader>ci", rhs = ":Inspect<CR>", desc = "Inspect character", category = "General" })
-add({ mode = "n", lhs = "<leader>sv", rhs = ":SSave default<CR>", desc = "Save session", category = "Session" })
-add({ mode = "n", lhs = "<leader>sl", rhs = ":SLoad default<CR>", desc = "Load session", category = "Session" })
+set("n", "<leader>ci", ":Inspect<CR>")
+set("n", "<leader>sv", ":SSave default<CR>")
+set("n", "<leader>sl", ":SLoad default<CR>")
 
 -- Diffview
-add({
-	mode = "n",
-	lhs = "<leader>ds",
-	rhs = ":lua require('utils').toggle_diffview_status()<CR>",
-	desc = "Toggle diffview status",
-	category = "Diffview",
-})
-add({
-	mode = "n",
-	lhs = "<leader>db",
-	rhs = ":lua require('utils').toggle_diffview_branch()<CR>",
-	desc = "Toggle diffview branch",
-	category = "Diffview",
-})
-add({
-	mode = "n",
-	lhs = "<leader>dh",
-	rhs = ":DiffviewFileHistory %<CR>",
-	desc = "Diffview file history",
-	category = "Diffview",
-})
+set("n", "<leader>ds", ":lua require('utils').toggle_diffview_status()<CR>")
+set("n", "<leader>db", ":lua require('utils').toggle_diffview_branch()<CR>")
+set("n", "<leader>dh", ":DiffviewFileHistory %<CR>")
 
 set("n", "<leader>dvfh", "<cmd>DiffviewFileHistory %<cr>")
 
@@ -157,16 +64,10 @@ set("n", "<leader>cpe", "<cmd>Copilot enable<cr>")
 set("n", "<leader>cpd", "<cmd>Copilot disable<cr>")
 
 -- Aerial
-add({ mode = "n", lhs = "<leader>ae", rhs = ":AerialToggle<cr>", desc = "Toggle Aerial", category = "Aerial" })
+set("n", "<leader>ae", ":AerialToggle<cr>")
 
 -- Spectre
-add({
-	mode = "n",
-	lhs = "<leader>sr",
-	rhs = ":lua require('spectre').toggle()<cr>",
-	desc = "Search and replace",
-	category = "Spectre",
-})
+set("n", "<leader>sr", ":lua require('spectre').toggle()<cr>")
 
 -- Git
 set("n", "<leader>hr", gs.reset_hunk)
--- lua/plugins/selfhelp.lua
+++
@@ -1,6 +0,0 @@
-local M = {
-	-- dir = "~/dev/nvim-plugins/selfhelp.nvim",
-	"jtubbenhauer/selfhelp.nvim",
-}
-
-return M
