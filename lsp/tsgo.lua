0f60d289de02a17096c6935e021da61952fbbd8c
Jack Tubbenhauer <jack@tubbs.dev>
2026-05-08 11:38:30 +1000

    pretty broken

--- lsp/tsgo.lua
+++ lsp/tsgo.lua
@@ -1,3 +1,22 @@
 return {
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  handlers = {
+    ["textDocument/diagnostic"] = function(err, result, ctx)
+      if result and result.items then
+        result.items = vim.tbl_filter(function(d)
+          -- Filter namespace-style import "not callable" errors (TS7 vs TS5 compat)
+          if d.code == 2349 then
+            local related = d.relatedInformation or {}
+            for _, info in ipairs(related) do
+              if info.message:find("namespace%-style import") then
+                return false
+              end
+            end
+          end
+          return true
+        end, result.items)
+      end
+      return vim.lsp.diagnostic.on_diagnostic(err, result, ctx)
+    end,
+  },
 }
--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,63 +1,69 @@
 vim.pack.add({
-  'https://github.com/stevearc/conform.nvim',
+	"https://github.com/stevearc/conform.nvim",
 })
 
-require('conform').setup({
-  formatters_by_ft = {
-    asm = { "asmfmt" },
-    lua = { "stylua" },
-    astro = { "prettier" },
-    javascript = { "prettierd" },
-    typescript = { "prettierd" },
-    typescriptreact = { "prettier" },
-    markdown = { "prettierd" },
-    html = { "prettierd" },
-    elixir = { "mix" },
-    eelixir = { "mix" },
-    heex = { "mix" },
-    surface = { "mix" },
-    htmlangular = { "prettierd" },
-    css = { "prettierd" },
-    scss = { "prettierd" },
-    sass = { "prettierd" },
-    xml = { "xmlformatter" },
-    proto = { "clang-format" },
-    python = { "black" },
-    go = { "golines" },
-    json = { "prettierd" },
-    jsonc = { "prettierd" },
-    cpp = { "clang-format" },
-    yaml = { "prettierd" },
-    yml = { "prettierd" },
-    cs = { "csharpier" },
-    rust = { "rustfmt" },
-    svelte = { "prettierd" },
-  },
-  notify_on_error = false,
-  format_on_save = function(bufnr)
-    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      return
-    end
-    return { timeout_ms = 2000, lsp_format = "fallback" }
-  end,
+require("conform").setup({
+	formatters_by_ft = {
+		asm = { "asmfmt" },
+		lua = { "stylua" },
+		astro = { "prettier" },
+		javascript = { "prettierd" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettier" },
+		markdown = { "prettierd" },
+		html = { "prettierd" },
+		elixir = { "mix" },
+		eelixir = { "mix" },
+		heex = { "mix" },
+		surface = { "mix" },
+		htmlangular = { "prettierd" },
+		css = { "prettierd" },
+		scss = { "prettierd" },
+		sass = { "prettierd" },
+		xml = { "xmlformatter" },
+		proto = { "clang-format" },
+		python = { "black" },
+		go = { "golines" },
+		json = { "prettierd" },
+		jsonc = { "prettierd" },
+		cpp = { "clang-format" },
+		yaml = { "prettierd" },
+		yml = { "prettierd" },
+		cs = { "csharpier" },
+		rust = { "rustfmt" },
+		svelte = { "prettierd" },
+		sql = { "sql_formatter" },
+		["*"] = { "injected" },
+	},
+	notify_on_error = false,
+	format_on_save = function(bufnr)
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 2000, lsp_format = "fallback" }
+	end,
 })
+
+require("conform").formatters.sql_formatter = {
+	prepend_args = { "-c", vim.fn.expand("~/.config/sql_formatter.json") },
+}
 
 vim.api.nvim_create_user_command("FormatDisable", function(args)
-  if args.bang then
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
+	if args.bang then
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
 end, { desc = "Disable autoformat-on-save", bang = true })
 
 vim.api.nvim_create_user_command("FormatEnable", function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
 end, { desc = "Re-enable autoformat-on-save" })
 
 vim.api.nvim_create_user_command("ToggleFormatOnSave", function()
-  vim.g.format_on_save = not vim.g.format_on_save
-  print("Format on save " .. (vim.g.format_on_save and "enabled" or "disabled"))
+	vim.g.format_on_save = not vim.g.format_on_save
+	print("Format on save " .. (vim.g.format_on_save and "enabled" or "disabled"))
 end, { desc = "Toggle format on save" })
 
 vim.keymap.set("n", "<leader>ft", ":ToggleFormatOnSave<CR>")
